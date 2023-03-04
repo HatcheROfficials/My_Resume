3049839357b9c814e95bbd4a20ff8362cd32d465
HatcheROfficials <hatcher.officials@gmail.com>
2023-03-04 20:06:20 +0530

    Added About Me and Experience Section
    Added details in About Me and Experience section.
    Need to optimize the pages for mobile.

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ var hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click",navMenuHideShow);
 // Implement Dropdown Menu CSS - END
 
+// Make selection from Nav Menu
 function navMenuItemSelect(event){
-    var navMenuContainer = document.querySelector(".topNav");
     var navMenuList = document.querySelectorAll(".topNav li");
 
     for(var i=0; i<navMenuList.length; i++){
@@ -25,12 +25,29 @@ function navMenuItemSelect(event){
     currentActive.classList.remove("active");
     event.target.classList.add("active");
 
-    // console.log(currentActive);
-    // console.log(event.target);
-    // navMenuContainer.insertBefore(event.target,currentActive);
+    displayActiveArticle(event.target.getAttribute("data-id"));
 }
 
 var navMenuItem = document.querySelectorAll(".topNav li:not(.hamburger)");
 navMenuItem.forEach((navItem)=>{
     navItem.addEventListener("click",navMenuItemSelect);
 });
+
+// Display active content in the content area
+function displayActiveArticle(activeDataId){
+    var articles = document.querySelectorAll("#content>article");
+
+    for(var i=0; i<articles.length; i++){
+        if(articles[i].getAttribute("data-id") == activeDataId){
+            articles[i].style.display = "";
+            articles[i].style.width = "100%";
+            articles[i].style.height = "100%";
+        } else{
+            articles[i].style.display = "none";
+        }
+    }
+}
+
+// Display a default content onload
+window.addEventListener("load",displayActiveArticle(1));
+
