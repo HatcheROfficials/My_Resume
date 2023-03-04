6b3b6f2144f46e493d3237818e6bad68cd26bd39
HatcheROfficials <hatcher.officials@gmail.com>
2023-03-04 08:33:54 +0530

    Implemented Nav Bar for Mobile and Desktop
    Improved UI
    Implemented Nav bar for mobile and desktop

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Implement Dropdown Menu CSS - START
 function navMenuHideShow(){
     var navMenu = document.querySelector(".topNav");
     if(navMenu.className == "topNav"){
@@ -9,3 +10,27 @@ function navMenuHideShow(){
 
 var hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click",navMenuHideShow);
+// Implement Dropdown Menu CSS - END
+
+function navMenuItemSelect(event){
+    var navMenuContainer = document.querySelector(".topNav");
+    var navMenuList = document.querySelectorAll(".topNav li");
+
+    for(var i=0; i<navMenuList.length; i++){
+        if(navMenuList[i].classList.contains("active")){
+            var currentActive = navMenuList[i];
+        }
+    }
+    
+    currentActive.classList.remove("active");
+    event.target.classList.add("active");
+
+    // console.log(currentActive);
+    // console.log(event.target);
+    // navMenuContainer.insertBefore(event.target,currentActive);
+}
+
+var navMenuItem = document.querySelectorAll(".topNav li:not(.hamburger)");
+navMenuItem.forEach((navItem)=>{
+    navItem.addEventListener("click",navMenuItemSelect);
+});
